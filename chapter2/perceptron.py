f92131225ea833207e97089e781b0516672ed55e
andy.sg <andy.sg@kakaoent.com>
2024-02-01 18:10:38 +0900

    first commit

---
+++ chapter2/perceptron.py
@@ -0,0 +1,6 @@
+import numpy as np
+x = np.array([0,1])
+w = np.array([0.5, 0.5])
+b = -0,7
+
+print(w * x)
---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('dataset/lena.png')
+
+plt.imshow(img)
+plt.show()
+
+# 밑바닥부터 시작하는 딥러닝1 repo
+# https://github.com/youbeebee/deeplearning_from_scratch
+
+# 밑바닥부터 시작하는 딥러닝2 repo
+# https://github.com/ExcelsiorCJH/DLFromScratch2
